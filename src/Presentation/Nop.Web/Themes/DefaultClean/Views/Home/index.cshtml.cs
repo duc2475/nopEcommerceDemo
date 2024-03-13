8253b227df8801801c4caa669d6ae1fd487ab7ca
duc2475 <doanduc333@gmail.com>
2024-03-13 15:42:47 +0700

    fisrt commit

---
+++ src/Presentation/Nop.Web/Themes/DefaultClean/Views/Home/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nop.Web.Themes.DefaultClean.Views.Home
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
